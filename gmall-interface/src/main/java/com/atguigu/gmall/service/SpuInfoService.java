86f686c019da3dda9eee6d6e511172ee66b4d335
zhoukaile <1105177248@qq.com>
2018-07-05 14:50:40 +0800

    -m里面放的是service接口

---
+++ gmall-interface/src/main/java/com/atguigu/gmall/service/SpuInfoService.java
@@ -0,0 +1,10 @@
+package com.atguigu.gmall.service;
+
+import com.atguigu.gmall.bean.SpuInfo;
+
+import java.util.List;
+
+public interface SpuInfoService {
+
+    List<SpuInfo> getSpuInfoList(SpuInfo spuInfo);
+}
